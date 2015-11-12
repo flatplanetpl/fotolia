42e50118a0864216362ff02c5ca3c5c3a7676b31
Damian Jarosch <damian@flatplanet.pl>
2015-11-12 13:22:57 +0100

    init

---
+++ demo.xamarin.forms/demo.xamarin.forms.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using demo.xamarin.forms.WinPhone.Resources;
+
+namespace demo.xamarin.forms.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
